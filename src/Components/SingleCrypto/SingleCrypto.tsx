3331f691daaf6a57097601cf379860337f9396e1
Yari PL <yaroslav.khudchenko@gmail.com>
2023-03-18 18:37:35 +0100

    add components for table and singleCrypto

---
+++ src/Components/SingleCrypto/SingleCrypto.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+interface Props {}
+
+const SingleCrypto: FunctionComponent<Props> = (): JSX.Element => {
+  return <section></section>;
+};
+
+export default SingleCrypto;
---
+++ src/Components/Table/Table.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+interface Props {}
+
+const Table: FunctionComponent<Props> = (): JSX.Element => {
+  return <section></section>;
+};
+
+export default Table;
